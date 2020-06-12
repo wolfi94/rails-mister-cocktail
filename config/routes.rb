85c5b08997c8acaacfd211e27adff3093df5b900
Wolfgang Alkier <w.alkier@gmx.at>
2020-06-12 13:11:58 +0200

    f

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :cocktails do
+  root to: 'cocktails#index'
+  resources :cocktails, except: [:index] do
     resources :doses, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
