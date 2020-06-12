298f02d445ac900f3de846e642f783877e603cfe
Wolfgang Alkier <w.alkier@gmx.at>
2020-06-12 15:11:50 +0200

    f

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -18,7 +18,7 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
     @cocktail.save
-    redirect_to cocktails_path
+    redirect_to root_path
   end
 
   def update
