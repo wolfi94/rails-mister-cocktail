d3404473f11ba18f2f3ab22abbbbf57f31cfb3eb
Wolfgang Alkier <w.alkier@gmx.at>
2020-06-12 12:54:58 +0200

    f

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -11,14 +11,14 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.new
   end
 
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
   def create
     @cocktail = Cocktail.new(cocktail_params)
     @cocktail.save
     redirect_to cocktails_path
-  end
-
-  def edit
-    @cocktail = Cocktail.find(params[:id])
   end
 
   def update
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -13,6 +13,15 @@ class DosesController < ApplicationController
      else
       render :new
   end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
   end
